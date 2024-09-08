2293e72b444d301a28719825b0b9f1d727450fe1
daniel <contractor@lateralaspect.com.au>
2024-09-08 10:23:52 +0800

    Update service-worker.php

--- inc/service-worker.php
+++ inc/service-worker.php
@@ -2,7 +2,7 @@
 
 function boot_register_my_service_worker() {   
     echo '<link rel="manifest" href="'.get_template_directory_uri().'/manifest.json">';
-    echo '<link rel="apple-touch-icon" href="'.get_template_directory_uri().'assets/images/pwa-logo-small.png">';
+    echo '<link rel="apple-touch-icon" href="'.get_template_directory_uri().'assets/images/pwa-logo-small.webp">';
     echo '<script>navigator.serviceWorker.register("'.get_template_directory_uri().'/service-worker.js")</script>';
 }
 add_action( 'wp_head', 'boot_register_my_service_worker' );
