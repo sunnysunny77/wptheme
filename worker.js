1f08f1f730895a08f05eac1b2c4faf50023b7324
daniel <contractor@lateralaspect.com.au>
2024-09-10 16:11:12 +0800

    up

--- worker.js
+++ worker.js
@@ -1,41 +1,42 @@
-const version = "v1";
+const version = 1;
+const cacheName = `wptheme-v${version}`;
 
-const addResourcesToCache = async (resources) => {
-  
-  const cache = await caches.open(version);
-  await cache.addAll(resources);
-};
+const cacheAssets = [
+  "./assets/css/app.min.css",
+  "./assets/js/app.min.js"
+];
 
 self.addEventListener("install", (event) => {
-
-  console.log(`${version} installing...`);
+  console.log("Service worker is installed");
 
   event.waitUntil(
-    addResourcesToCache([
-
-      "./assets/css/app.min.css",
-      "./assets/js/app.min.js"
-    ])
+    caches
+      .open(cacheName)
+      .then((cache) => {
+        console.log("Caching assets");
+        cache.addAll(cacheAssets);
+      })
+      .then(() => self.skipWaiting())
   );
 });
 
 self.addEventListener("fetch", (event) => {
-    console.log("Fetching via Service worker");
-  
-    if (event.request.url.match(/wp-admin/) || event.request.url.match(/preview=true/)) {
-      return;
-      }
+  console.log("Fetching via Service worker");
 
-    event.respondWith(
-      fetch(event.request)
-        .then((networkResponse) => {
-          return caches.open(version).then((cache) => {
-            cache.put(event.request, networkResponse.clone());
-            return networkResponse;
-          });
-        })
-        .catch(() => {
-          return caches.match(event.request);
-        })
-    );
-  });
+  if (event.request.url.match(/wp-admin/) || event.request.url.match(/preview=true/)) {
+    return;
+  }
+
+  event.respondWith(
+    fetch(event.request)
+      .then((networkResponse) => {
+        return caches.open(cacheName).then((cache) => {
+          cache.put(event.request, networkResponse.clone());
+          return networkResponse;
+        });
+      })
+      .catch(() => {
+        return caches.match(event.request);
+      })
+  );
+});
