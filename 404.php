e1c63b00c67cd4a4fd02d3f4ac384666787efb0c
sunnysunny77 <46119793+sunnysunny77@users.noreply.github.com>
2025-12-17 21:14:43 +0800

    up

--- 404.php
+++ 404.php
@@ -1,16 +1,18 @@
 <?php get_header(); ?>
 
-	<main id="main" class="container">
+	<main id="main" class="container template py-11">
 
-		<h1> 404 not found</h1>
+		<h1 class="w-100 text-center mb-7"> 404 not found</h1>
 		
-		<h2> :(</h2>
+		<h2 class="mb-5"> :(</h2>
 
-		<p>
+		<p class="pb-6 mb-4">
 			Regretfully this page could not be found.
 			<br>
 			Untill we find it, please try elsewhere.
 		</p>
+
+		<hr class="mb-6 mx-2"/>
 
 		<?php get_search_form(); ?>
 
